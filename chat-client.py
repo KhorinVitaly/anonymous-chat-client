5e0d64a42edd95ff173aff9f18950bd3d648b3c7
Khorin Vitaly <khorin.v@gmail.com>
2020-03-10 23:05:20 +0600

    Initial commit

--- chat-client.py
+++ chat-client.py
@@ -2,6 +2,7 @@ import asyncio
 import gui
 import datetime
 import configargparse
+from aiofile import AIOFile
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -10,14 +11,16 @@ async def main(args):
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
+    history_queue = asyncio.Queue()
     await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
-        read_msgs(messages_queue, args)
+        read_msgs(messages_queue, history_queue, args.host, args.port),
+        save_msgs(args.history, history_queue)
     )
 
 
-async def read_msgs(messages_queue, args):
-    reader, writer = await asyncio.open_connection(args.host, args.port)
+async def read_msgs(messages_queue, history_queue, host, port):
+    reader, writer = await asyncio.open_connection(host, port)
     while True:
         data = await reader.readline()
         if not data:
@@ -25,7 +28,26 @@ async def read_msgs(messages_queue, args):
         str_datetime = datetime.datetime.now().strftime("%d %m %Y %H:%M:%S")
         message = f'{str_datetime} {data.decode()}'
         messages_queue.put_nowait(message)
-        await asyncio.sleep(1)
+        history_queue.put_nowait(message)
+
+
+async def save_msgs(filepath, history_queue):
+    while True:
+        message = await history_queue.get()
+        async with AIOFile(filepath, 'a') as afp:
+            await afp.write(message)
+
+
+async def send_msgs(sending_queue, host, port):
+    while True:
+        message = await sending_queue.get()
+        reader, writer = await asyncio.open_connection(host, port)
+
+
+async def readline(reader):
+    data = await reader.readline()
+    text = data.decode()
+    return text
 
 
 if __name__ == '__main__':
